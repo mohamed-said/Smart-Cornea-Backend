c91a90243f5bdf945604c5caab5fdcb08a026699
Kogo <mohamed.said.fci@gmail.com>
2016-06-17 17:18:51 +0200

    First Commit

---
+++ smartcornea/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmartcorneaConfig(AppConfig):
+    name = 'smartcornea'
---
+++ smartcornea/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+import cv2
+
+# Create your views here.
